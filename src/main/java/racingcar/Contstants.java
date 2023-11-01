df7df747a6b524e92feb46b84da1254529739ed6
barobon <46195201+barobon@users.noreply.github.com>
2023-11-01 16:36:31 +0900

    feat(constants): constant 클래스 구현
    상수들을 관리하는 클래스 구현
    텍스트 출력에 사용되는 메세지들을 상수로 관리

---
+++ src/main/java/racingcar/Contstants.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+public class Contstants {
+    public static String ASK_PLAYERS_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static String ASK_RACE_NUMBERS_MESSAGE = "시도할 회수는 몇회인가요?";
+    public static String RACE_RESULT_MESSAGE = "실행 결과";
+    public static String COLON = " : ";
+    public static String SCORE_TEXT = "-";
+    public static String LAST_WINNER_MESSAGE = "최종 우승자";
+
+
+}
